6f1dea089ecd8389c3a74a77e1e44f707c2e938e
sammyvie <samanthagonzales1003@gmail.com>
2026-02-26 13:48:48 +0800

    Import CustomizeDemo and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { TemplateDemo } from './template-demo/template-demo';
 import { ReactiveDemo } from './reactive-demo/reactive-demo';
-import { CustomizeDemo } from './custome/customize-demo'; // <--- PUT IT HERE
+import { CustomizeDemo } from './custome/customize-demo'; 
 
 export const routes: Routes = [
   { path: 'template', component: TemplateDemo },
   { path: 'reactive', component: ReactiveDemo },
-  { path: 'custom', component: CustomizeDemo }, // <--- AND UPDATE THIS
+  { path: 'custom', component: CustomizeDemo }, 
   { path: '', redirectTo: '/template', pathMatch: 'full' }
 ];
